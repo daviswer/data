fa05b350390866a6d2970c55c21964d3dced22b1
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-05 23:41:02 -0500

    Make it importable

--- torchdata/scalable_reader/__init__.py
+++ torchdata/scalable_reader/__init__.py
@@ -4,13 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .base_loader import ScalableReader, _StatefulDataset
+from .base_loader import ScalableReader, ScalableHFReader, _StatefulDataset
 from .wrappers import PreprocessDataset,ShuffleDataset,DocPackingDataset,SamplingDataset,_NestedStatefulDataset
 from .file_handlers import ShardFileHandler, ArrowHandler, ParquetHandler
 from .dcp_utils import save_ckpt_dcp, load_ckpt_dcp
 
 __all__ = [
     "ScalableReader",
+    "ScalableHFReader",
     "PreprocessDataset",
     "ShuffleDataset",
     "DocPackingDataset",
