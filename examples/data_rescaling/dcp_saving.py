e6fecabe0cb7700d50a8007e4b1ff88074f4c263
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-17 17:52:27 -0500

    Named meshes

--- examples/data_rescaling/dcp_saving.py
+++ examples/data_rescaling/dcp_saving.py
@@ -40,7 +40,7 @@ args = parser.parse_args()
 rank = int(os.getenv("RANK", 0))
 world_size = int(os.getenv("WORLD_SIZE", 1))
 dist.init_process_group(backend="gloo")
-mesh = dist.device_mesh.init_device_mesh("cpu", [world_size//args.cp_degree, args.cp_degree])
+mesh = dist.device_mesh.init_device_mesh("cpu", [world_size//args.cp_degree, args.cp_degree], mesh_dim_names=["dp","cp"])
 
 # Check input args
 assert args.logical_shards >= world_size*args.num_workers, f"Logical shards {args.logical_shards} cannot be less than total workers {world_size*args.num_workers}"
