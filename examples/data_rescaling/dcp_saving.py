5950d6f2d7896192d7b361fce7a4d8bc68645c81
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-12-17 15:45:33 -0500

    Instantiate fhandler

--- examples/data_rescaling/dcp_saving.py
+++ examples/data_rescaling/dcp_saving.py
@@ -72,7 +72,7 @@ if not os.path.exists(datapath):
         time.sleep(5)
 
 # Build dataloader
-data = ScalableReader(datapath, rank//args.cp_degree, world_size//args.cp_degree, ArrowHandler, -1, seed=args.seed, max_chunksize=40, n_logical_shards=args.logical_shards)
+data = ScalableReader(datapath, rank//args.cp_degree, world_size//args.cp_degree, ArrowHandler(), -1, seed=args.seed, max_chunksize=40, n_logical_shards=args.logical_shards)
 # Subdata sampling
 data = SamplingDataset(datapath, data, -1, ["subdata","subfolder"], [2,1])
 # Packing and slicing
