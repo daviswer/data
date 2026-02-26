0ae6235948874ab3dbe7ed3cde47eeafb6b3910b
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-02-26 17:02:28 -0500

    Construct_reader arg passing

--- torchdata/scalable_reader/base_loader.py
+++ torchdata/scalable_reader/base_loader.py
@@ -275,7 +275,7 @@ class ScalableTitanMMReader(_StatefulDataset):
                 # to the end of self.shard_states after it is exhausted
                 i = k-j
                 shardid = self._shard_manager.get_shard_id(i)
-                self.construct_reader(shardid, self.n_logical_shards, self.shard_states[i])
+                self.construct_reader(shardid, self.n_logical_shards)
                 reader = iter(self.current_stream)
                 # For each shard, iterate through all the remaining docs
                 self.current_shard = i
