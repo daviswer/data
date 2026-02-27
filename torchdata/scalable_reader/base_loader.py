d2ab9f6b70372d98ed858620b3a73e9be8d72fa4
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-02-27 00:41:11 -0500

    Verbose loading

--- torchdata/scalable_reader/base_loader.py
+++ torchdata/scalable_reader/base_loader.py
@@ -144,6 +144,8 @@ class _StatefulDataset(data.IterableDataset):
             ["state", "broadcast", "reshard", "custom"],
             [self.state_vars, self.broadcast_vars, self.reshard_vars, self.custom_vars],
         ):
+            if state_type=="custom":
+                print(f".   Rank {self.rank} custom vars {self.state_dict["custom"]}")
             [setattr(self, flag, state_dict[state_type][self.statename(flag)]) for flag in flags]
         # Apply custom reshard fns to loaded custom values
         if state_dict["custom"]["__rescaling__"]:
