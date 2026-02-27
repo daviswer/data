e833a746826ce28a920b4ccbb17779bb7f325d20
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-02-27 01:27:29 -0500

    Diag load meta d

--- torchdata/scalable_reader/dcp/load.py
+++ torchdata/scalable_reader/dcp/load.py
@@ -51,6 +51,7 @@ def load_ckpt_dcp(
 
     # Get checkpoint metadata
     meta = get_checkpoint_metadata(path)
+    print(f".   Meta dict for custom loading: {meta["custom"]}")
 
     # Load each category
     if easy_load:
