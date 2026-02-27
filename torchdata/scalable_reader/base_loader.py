d887af8c97efc22a358d19f0d32fa74ba4206da6
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-02-27 12:44:28 -0500

    Swap out random seed for diag porpoises

--- torchdata/scalable_reader/base_loader.py
+++ torchdata/scalable_reader/base_loader.py
@@ -168,7 +168,7 @@ class ScalableTitanMMReader(_StatefulDataset):
         rank: int,
         worldsize: int,
         n_logical_shards: int = 30720,
-        seed: int = 42,
+        seed: int = 421,
     ):
         super().__init__("Dummy", rank, worldsize)
         self.data_constructor = partial(data_constructor, infinite=False)
