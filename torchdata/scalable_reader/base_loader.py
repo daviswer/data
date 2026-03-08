f2c21b40c550bef032561d9828ebb39889697b9e
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-03-07 22:33:25 -0500

    Fix diag print

--- torchdata/scalable_reader/base_loader.py
+++ torchdata/scalable_reader/base_loader.py
@@ -422,8 +422,8 @@ class ScalableMMReader(_StatefulDataset):
         TODO
         """
         # Map rank to underlying shuffled index
+        print(f".   Rank {self.rank}: {self.shard_states}, {self._shard_manager.get_shard_id(rank)}")
         rank = self._shard_manager.get_shuffled_shard_id(rank)
-        print(f".   Rank {self.rank}: {self.shard_states}, {rank}")
         # Fetch relevant HF data shard
         reader = split_dataset_by_node(self.data, rank, self.n_logical_shards)
         d = reader.state_dict()
