ede1d5c381967ecc3946a8112978118e4223a9ff
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-03-07 20:46:33 -0500

    Fix fstring

--- torchdata/scalable_reader/base_loader.py
+++ torchdata/scalable_reader/base_loader.py
@@ -457,7 +457,7 @@ class ScalableMMReader(_StatefulDataset):
                             continue
                         if out["input_ids"].shape[0] > self.max_seq_len:
                             print(
-                                f"Rank {self.rank}: Sample length {out["input_ids"].shape[0]} > training {self.max_seq_len}. Skip"
+                                f"Rank {self.rank}: Sample length {out['input_ids'].shape[0]} > training {self.max_seq_len}. Skip"
                             )
                             continue
                         yield out
