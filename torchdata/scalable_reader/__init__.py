b150095d1011ce366097f3ff776db057bf3076b2
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-03-12 17:20:31 -0400

    Fix imports

--- torchdata/scalable_reader/__init__.py
+++ torchdata/scalable_reader/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .base_loader import ScalableReader, ScalableHFReader, ScalableMMReader, _StatefulDataset
+from .base_loader import ScalableReader, ScalableHFReader, _StatefulDataset
 from .wrappers import PreprocessDataset,ShuffleDataset,DocPackingDataset,SamplingDataset,TitanMMPackingDataset,_NestedStatefulDataset
 from .file_handlers import ShardFileHandler, ArrowHandler, ParquetHandler
 from .dcp_utils import save_ckpt_dcp, load_ckpt_dcp
@@ -14,7 +14,6 @@ from .shard_rescaler import atomic_rescale, epoch_balanced_rescale
 __all__ = [
     "ScalableReader",
     "ScalableHFReader",
-    "ScalableMMReader",
     "PreprocessDataset",
     "ShuffleDataset",
     "DocPackingDataset",
