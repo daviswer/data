62b805d58eaffc92e887cb7713ac6a5fb3fc961f
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-03-24 01:02:13 -0400

    Diag print

--- torchdata/scalable_reader/wrappers.py
+++ torchdata/scalable_reader/wrappers.py
@@ -362,7 +362,7 @@ class DictShuffleDataset(_NestedStatefulDataset):
         self.g_state = self.generator.get_state().clone().tolist()
         # Pull buffer fields into reshard vars
         buffer = self.buffer[:self.buffer_size]
-        print(f".   Rank {self.rank}: {self.data_keys, self.buffer[0]}")
+        print(f".   Rank {self.rank}: {self.data_keys, self.buffer[:1]}")
         for i in range(self.n_data_fields):
             buffer_i = [x[self.data_keys[i]] for x in buffer]
             setattr(self, "buffer_"+str(i), buffer_i)
