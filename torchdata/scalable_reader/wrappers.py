cec19c582f4a3d2b6b14f4c645330d511eab527a
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-04-10 01:00:34 -0400

    Diag print

--- torchdata/scalable_reader/wrappers.py
+++ torchdata/scalable_reader/wrappers.py
@@ -332,7 +332,7 @@ class DictShuffleDataset(_NestedStatefulDataset):
             self.buffer = []
             self.buffer_size = 0
             self._pad_buffer()
-        print(f".   Rank {self.rank}: beginning iteration, {self.buffer_size}, {self.buffer[:self.buffer_size]}")
+        print(f".   Rank {self.rank}: beginning iteration, {self.buffer_size}")
         while True:
             # If buffer is undersized, add a datapoint
             if self.buffer_size < self.window_size:
@@ -350,6 +350,7 @@ class DictShuffleDataset(_NestedStatefulDataset):
                 # If buffer is small, add new item into the freed slot.
                 self.buffer[i] = first_draw if first_draw is not None else next(dataset)
                 first_draw = None
+            print(f".   Rank {self.rank}: yielding")
             yield out
 
     def _pad_buffer(self):
