e8de928e7432d1fcb72273db85dabf0e06e7c2e7
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-04-09 23:03:22 -0400

    Check rescaling by var type not val

--- torchdata/scalable_reader/wrappers.py
+++ torchdata/scalable_reader/wrappers.py
@@ -675,15 +675,12 @@ class TitanMMPackingDataset(_NestedStatefulDataset):
         return super().state_dict()
     
     def load_state_dict(self, state_dict):
-        self.packer_buffers_state = None
-        self.packer_samples_state = None
         super().load_state_dict(state_dict)
-        if self.packer_buffers_state is None:
+        if not isinstance(self.packer_buffers_state, List):
             # If not rescaling, unpickle list-valued state vars
             self.packer_buffers_state = pickle.loads(self.packer_buffers_state)
             self.packer_samples_state = pickle.loads(self.packer_samples_state)
         else:
-            print(self.packer_buffers_state)
             # If rescaling, pickle_atomic_rescale returns a list of states. 
             # Extract/merge relevant list entries
             def list_state_handler(state):
